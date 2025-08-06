433ef469e2d6b27d40a5bf38e8b39a1db95a09a1
Lewis Howles <lewishowles@users.noreply.github.com>
2025-08-06 13:44:41 +0100

    Correct base URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-	base: "/form-builder/",
+	base: "/tool-form-builder/",
 	plugins: [
 		vue(),
 		vueDevTools(),
